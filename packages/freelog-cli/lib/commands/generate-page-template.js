bf679a8179df3344efb625904b2ca764af4bd6c8
liu-kai <31062816+liu-kai-github@users.noreply.github.com>
2020-05-13 10:35:17 +0800

    Update generate-page-template.js

--- packages/freelog-cli/lib/commands/generate-page-template.js
+++ packages/freelog-cli/lib/commands/generate-page-template.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const main = async () => {
 
     const {data: templateString} = await axios.get(aliyuncsPagebuildUrl);
-    
+
     const html = templateString
       .replace(/(?<=<div id="js-page-container">)[\s\S]*?(?=<\/div>)/, `<%= require('html-loader!./them-template.html') %>`)
       .replace('{{authInfoFragment | safe}}', `<%= require('html-loader!./__auth_info__.html') %>`)
