8d2df4afa5e03eed83fce4a1c91d31407ef65268
liu-kai <31062816+liu-kai-github@users.noreply.github.com>
2020-06-09 15:05:55 +0800

    更换工具

--- packages/create-freelog-app/src/generate.ts
+++ packages/create-freelog-app/src/generate.ts
@@ -1,5 +1,6 @@
 import * as  Metalsmith from 'metalsmith';
-import {handlebars} from 'consolidate';
+// import {handlebars} from 'consolidate';
+import * as Mustache from 'mustache';
 
 export default function (source: string, destination: string, metadata: any) {
   return new Promise((resolve, reject) => {
@@ -24,12 +25,14 @@ function template(files: any, metalsmith: any, done: any) {
 
   for (const file of keys) {
     var str = files[file].contents.toString();
-    handlebars.render(str, metadata, function (err: any, res: any) {
-      if (err) {
-        return done(err);
-      }
-      files[file].contents = new Buffer(res);
-    });
+    // handlebars.render(str, metadata, function (err: any, res: any) {
+    //   if (err) {
+    //     return done(err);
+    //   }
+    //   files[file].contents = new Buffer(res);
+    // });
+    const output = Mustache.render(str, metadata);
+    files[file].contents = new Buffer(output);
   }
   done();
 }
